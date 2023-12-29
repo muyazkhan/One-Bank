4c54b32a9a9f1042370238960e462aec47bf8524
Muyaz khan <muyazkhan06@gmail.com>
2023-12-29 17:18:42 +0600

    reuplode

--- Digital_Bank/settings.py
+++ Digital_Bank/settings.py
@@ -9,11 +9,13 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+from pathlib import Path
 import dj_database_url
 import environ
+import dj_database_url
 env = environ.Env()
 environ.Env.read_env()
-from pathlib import Path
+# from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
