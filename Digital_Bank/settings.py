347111e1bb7cc49a5b68135b6f34c83f7b76ea9a
Muyaz khan <muyazkhan06@gmail.com>
2023-12-29 17:28:04 +0600

    reuplode

--- Digital_Bank/settings.py
+++ Digital_Bank/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import dj_database_url
 import environ
-import dj_database_url
 env = environ.Env()
 environ.Env.read_env()
 # from pathlib import Path
